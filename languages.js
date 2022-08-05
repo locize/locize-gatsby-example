c68030269308933394266b980c9ab7c6892bb52f
Adriano Raiano <adriano@raiano.ch>
2022-08-05 19:40:37 +0200

    some comments

--- languages.js
+++ languages.js
@@ -2,11 +2,14 @@ const { join } = require('path')
 const { readdirSync, lstatSync } = require('fs')
 
 const defaultLanguage = 'en';
+
+// based on the directories get the language codes
 const languages = readdirSync(join(__dirname, 'locales')).filter((fileName) => {
   const joinedPath = join(join(__dirname, 'locales'), fileName)
   const isDirectory = lstatSync(joinedPath).isDirectory()
   return isDirectory
 });
+// defaultLanguage as first
 languages.splice(languages.indexOf(defaultLanguage), 1);
 languages.unshift(defaultLanguage);
 
